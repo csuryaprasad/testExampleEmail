11ef170d00cf2184624d0bc5122cb02f57c364e5
csuryaprasad <csuryaprasad@gmail.com>
2022-04-05 17:56:02 -0400

    updated

--- src/test/java/EmailTest/testEmail.java
+++ src/test/java/EmailTest/testEmail.java
@@ -59,13 +59,13 @@ public class testEmail {
 		 if(ITestResult.FAILURE==result.getStatus())   	{
 			 try {
 				 	log.info("connected to the Linux ENV");
-				 	TakesScreenshot ts=(TakesScreenshot)driver;
-				  	File source=ts.getScreenshotAs(OutputType.FILE);
-				  	log.info("screen shot "+source);
-				  	log.info(result.getName());
+				 	//TakesScreenshot ts=(TakesScreenshot)driver;
+				  	//File source=ts.getScreenshotAs(OutputType.FILE);
+				  	//log.info("screen shot "+source);
+				  	//log.info(result.getName());
 					//FileHandler.copy(source, new File("./test-output/"+result.getName()+"12.png"));
 					//log.info("Screenshot taken");
-					String path = System.getProperty("user.dir")+"\\test-output";
+					String path = System.getProperty("user.dir")+"//test-output";
 				  	log.info(path);
 				  	final File folder = new File(path);
 				  	log.info("folderName-"+folder.getName());
